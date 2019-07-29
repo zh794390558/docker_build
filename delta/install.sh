47c26bc5c3519f312df5829584b4f3bfc2c59407
Hui Zhang <ethanzhanghui@didichuxing.com>
2019-07-29 11:24:02 +0800

    fix delta dockerfile

---
+++ delta/install.sh
@@ -0,0 +1,13 @@
+apt-get update && apt-get install -y \
+        autoconf\
+        automake \
+        clang-format \
+        curl \
+        git \
+        libtool \
+        sudo \
+        sox \
+        tig \
+        make \
+        vim \
+        unzip
